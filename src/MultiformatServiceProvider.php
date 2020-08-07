df9466c3f18ad5c5c4830497e67fe48e10789068
Patrick Tournet <ptournet@users.noreply.github.com>
2020-08-07 22:26:40 +0200

    Modifications for Laravel 6+ compatibility

--- src/MultiformatServiceProvider.php
+++ src/MultiformatServiceProvider.php
@@ -4,7 +4,9 @@ namespace Ptournet\Multiformat;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class MultiformatServiceProvider extends ServiceProvider
 {
@@ -17,14 +19,20 @@ class MultiformatServiceProvider extends ServiceProvider
             return $this->setUri($this->uri().'{_format?}');
         });
 
-        Request::macro('match', function ($responses, $defaultFormat = 'html') {
+        Request::macro('matchFormat', function ($responses, $defaultFormat = 'html') {
             if ($this->route('_format') === null) {
-                return value(array_get($responses, $this->format($defaultFormat)));
+                return value(Arr::get($responses, $this->format($defaultFormat)));
             }
 
-            return value(array_get($responses, str_after($this->route('_format'), '.'), function () {
+            return value(Arr::get($responses, Str::after($this->route('_format'), '.'), function () {
                 abort(404);
             }));
+        });
+
+        Request::macro('match', function ($responses, $defaultFormat = 'html') {
+            echo "match() macro has been deprecated and will be removed in v2, please use matchFormat() instead.\n";
+
+            return $this->matchFormat($responses, $defaultFormat);
         });
     }
 
--- tests/MultiformatServiceProviderTest.php
+++ tests/MultiformatServiceProviderTest.php
@@ -85,4 +85,63 @@ class MultiformatServiceProviderTest extends TestCase
         $response = $this->get($uri.'.foo');
         $response->assertStatus(404);
     }
+
+
+    /** @test */
+    public function the_matchFormat_macro_should_return_the_correct_type_for_a_non_multiformat_endpoint_based_on_the_http_accept()
+    {
+        $uri = "initial/endpoint";
+        Route::get($uri, function (Request $request) {
+            return $request->matchFormat([
+                'html' => 1,
+                'json' => 2,
+                'xml' => 3
+            ]);
+        });
+
+        $response = $this->get($uri);
+        $this->assertEquals('1', $response->content());
+        $response = $this->get($uri, ['Accept' => 'application/json']);
+        $this->assertEquals('2', $response->content());
+        $response = $this->get($uri, ['Accept' => 'application/xml']);
+        $this->assertEquals('3', $response->content());
+    }
+
+    /** @test */
+    public function the_matchFormat_macro_should_return_the_correct_type_for_a_valid_format_for_a_multiformat_endpoint()
+    {
+        $uri = "initial/endpoint";
+        Route::get($uri, function (Request $request) {
+            return $request->matchFormat([
+                'html' => 1,
+                'json' => 2,
+                'xml' => 3
+            ]);
+        })->multiformat();
+
+        $response = $this->get($uri);
+        $this->assertEquals('1', $response->content());
+        $response = $this->get($uri.'.html');
+        $this->assertEquals('1', $response->content());
+        $response = $this->get($uri.'.json');
+        $this->assertEquals('2', $response->content());
+        $response = $this->get($uri.'.xml');
+        $this->assertEquals('3', $response->content());
+    }
+
+    /** @test */
+    public function the_matchFormat_macro_should_return_a_404_for_an_invalid_format_for_a_multiformat_endpoint()
+    {
+        $uri = "initial/endpoint";
+        Route::get($uri, function (Request $request) {
+            return $request->matchFormat([
+                'html' => 1,
+                'json' => 2,
+                'xml' => 3
+            ]);
+        })->multiformat();
+
+        $response = $this->get($uri.'.foo');
+        $response->assertStatus(404);
+    }
 }
